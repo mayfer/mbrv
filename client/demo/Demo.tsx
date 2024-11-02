66f767b690cad4541b5bdccfd769bfef8e401c75
Murat Ayfer <murat@ayfer.net>
2024-11-01 17:20:14 -0700

    redesign

--- client/demo/Demo.tsx
+++ client/demo/Demo.tsx
@@ -1,9 +1,23 @@
 import { useState, useEffect } from 'react';
 import { Greeting, SocketTester } from 'shared/types';
 import { MainProps } from 'shared/main_props';
-import { Button, Card, InnerCard, Code, Container, Title, Title2 } from './Elements';
+import { Button, Card, CardsContainer, Code, Container, Title,Title1, Title2 } from './Elements';
 import io from 'socket.io-client';
-import logo from '/client/assets/picnic.svg';
+import { GithubIcon } from './Icons';
+import styled from 'styled-components';
+import './Demo.css';
+
+const CloneGithub = styled.a`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  background: linear-gradient(to right, #efe57d, #95d560);
+  color: #333 !important;
+  padding: 5px 10px;
+  border-radius: 4px;
+  margin: 10px;
+  text-decoration: none !important;
+`;
 
 function Demo(mainProps: MainProps) {
   const greeting = mainProps.greeting;
@@ -28,81 +42,95 @@ function Demo(mainProps: MainProps) {
 
   return (
     <Container>
-      <img src={logo} height="100" /><br />
-      <Title2>Murat's Bun, React & Vite template</Title2>
-      <Title2><a href='https://github.com/mayfer/mbrv'>Clone on GitHub</a></Title2>
       <Title>MBRV</Title>
-      <Card>
-        <Title2>Single port convenience</Title2>
-        <ul>
-          <li>Server and client run on the same port</li>
-          <li>No proxies or CORS issues</li>
-        </ul>
+      <Title2>Murat's Bun, React & Vite template</Title2>
+      <Title2>Easiest way to Use TypeScript on both client & server</Title2>
+      <Title2>
+        <CloneGithub href='https://github.com/mayfer/mbrv'>
+          <GithubIcon /> Clone on GitHub
+        </CloneGithub>
+        </Title2>
 
-        <Title2>Bun's flexibility</Title2>
-        <ul>
-          <li>
-            Speed aside, <a href='https://bun.sh' target="_blank">bun</a> makes it a pleasure to run TypeScript without a transpiler
-          </li>
-          <li>
-            <Code>run.ts</Code> as single clear entry point
-          </li>
-        </ul>
 
-        <Title2>React hot module reloads</Title2>
-        <ul>
-          <li>Edit <Code>client/App.tsx</Code> and save to test React HMR</li>
-          <li>Server will also reload when <Code>server/</Code> or <Code>shared/</Code> changes, using nodemon to restart the bun process</li>
-        </ul>
-
-        <Title2>Shared files</Title2>
-        <ul>
-          <li>Server & client can use shared interfaces, types, and functions via <Code>shared/</Code> folder </li>
-        </ul>
-
-        <Title2>Sane server-side rendering</Title2>
-        <ul>
-          <li>SSR is done with explicit code, no framework magic: <Code>index.html</Code> is generated in <Code>run.ts</Code></li>
-          <li>Enabled by default, with entry point in <Code>server/ssr.tsx</Code></li>
-        </ul>
-
-        <Title2>Express & socket.io</Title2>
-        <ul>
-          <li>Express router in <Code>server/apis/http.ts</Code></li>
-          <li>Socket.io setup in <Code>server/apis/sockets.ts</Code></li>
-        </ul>
-        <InnerCard>
-          <Title2>Instructions</Title2>
+      <CardsContainer>
+        <Card>
+          <Title1>Benefits</Title1>
+          <Title2>Single port convenience</Title2>
           <ul>
-            <li><a href='https://github.com/mayfer/mbrv'>Clone on GitHub</a> & run <Code>bun install</Code></li>
-            <li>Make application-specific changes in <Code>client/</Code>, <Code>shared/</Code> & <Code>server/</Code> dirs</li>
-            <li><Code>bun dev</Code> provides hot reloads & source maps</li>
-            <li><Code>bun prod</Code> builds & serves production</li>
-            <li><Code>bun run.ts</Code> to run without any build process</li>
+            <li>Server & client run on the same process, same port</li>
+            <li>No proxies or CORS issues</li>
           </ul>
-        </InnerCard>
-      </Card>
 
-      <Card>
-        
-            <Title2>Test the HTTP API & SSR:</Title2>
-            <Button onClick={async () => {
-              setLoading(true);
-              const res = await fetch("/api/props");
-              const { greeting } = await res.json() as MainProps;
+          <Title2>Bun's flexibility</Title2>
+          <ul>
+            <li>
+              Speed aside, <a href='https://bun.sh' target="_blank">bun</a> makes it a pleasure to run TypeScript without a transpiler
+            </li>
+            <li>
+              <Code>run.ts</Code> as single entry point
+            </li>
+          </ul>
 
-              setServerGreeting(greeting);
-              setLoading(false);
-            }}>Fetch clock from server</Button>
-            {loading && <span> Loading...</span>}
+          <Title2>React hot module reloads</Title2>
+          <ul>
+            <li>UI code changes appear instantly in the browser</li>
+            <li>Page will reload if any server code is changed</li>
+          </ul>
 
-            <br />
-            Response: "{serverGreeting?.text}"
+          <Title2>Shared files</Title2>
+          <ul>
+            <li>Server & client can use shared TypeScript interfaces, types, and functions via <Code>shared/</Code> folder </li>
+          </ul>
 
-          <br /><br />
-          <Title2>Test Socket.io:</Title2>
-          Counter: {count}
-      </Card>
+          <Title2>Sane server-side rendering</Title2>
+          <ul>
+            <li>SSR is done with explicit code, no framework magic: <Code>index.html</Code> is generated in <Code>run.ts</Code></li>
+            <li>Enabled by default, with entry point in <Code>server/ssr.tsx</Code></li>
+          </ul>
+        </Card>
+        <Card>
+          <Title1>Instructions</Title1>
+          <Title2>Setup</Title2>
+          <ul>
+            <li><Code>bun i</Code> to install packages</li>
+            <li><Code>bun dev</Code> provides hot reloads & source maps</li>
+            <li><Code>bun build</Code> builds only (into <Code>dist/</Code>)</li>
+            <li><Code>bun prod</Code> serves production</li>
+          </ul>
+          <Title2>Make changes</Title2>
+          <ul>
+            <li><Code>client/</Code> contains React UI code</li>
+            <li><Code>shared/</Code> folder contains shared interfaces, types, and functions that both client & server can use</li>
+            <li><Code>server/</Code> contains http & socket APIs
+              <ul>
+                <li>Express endpoints in <Code>server/apis/http.ts</Code></li>
+                <li>Socket.io setup in <Code>server/apis/sockets.ts</Code></li>
+              </ul>
+            </li>
+          </ul>
+        </Card>
+
+        <Card>
+            <Title1>Demo</Title1>
+              <Title2>Test the HTTP API & SSR:</Title2>
+              <Button onClick={async () => {
+                setLoading(true);
+                const res = await fetch("/api/props");
+                const { greeting } = await res.json() as MainProps;
+
+                setServerGreeting(greeting);
+                setLoading(false);
+              }}>Fetch clock from server</Button>
+              {loading && <span> Loading...</span>}
+
+              <br />
+              Response: "{serverGreeting?.text}"
+
+            <br /><br />
+            <Title2>Test Socket.io:</Title2>
+            Counter: {count}
+        </Card>
+      </CardsContainer>
     </Container>
   );
 }
--- client/demo/Elements.tsx
+++ client/demo/Elements.tsx
@@ -9,49 +9,60 @@ export const Container = styled.div`
   min-height: 100vh;
   font-family: monospace;
   line-height: 1.6;
+  background-color: #333;
+  color: #aaa;
+
+  & a {
+    color: #fff;
+    text-decoration: underline;
+    &:hover {
+      text-shadow: 0 0 10px #fff;
+    }
+  }
+`;
+
+export const CardsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  width: 100%;
+  margin: 0 auto;
+  gap: 1px;
+`;
+
+export const Card = styled.div`
+  padding: 30px;
 `;
 
 export const Title = styled.h1`
-  font-family: Montserrat, sans-serif;
   font-size: 3rem;
-  color: #333;
-  text-shadow: 0 0px 30px rgba(0, 200, 0, 0.5);
 `;
+
+export const Title1 = styled.h1`
+  color: #fff;
+  text-shadow: 0 0px 30px rgba(255, 255, 255, 0.5);
+  font-size: 2rem;
+  margin: 0 0 20px 0;
+  padding: 0;
+`;
+
 export const Title2 = styled.h2`
+color: #fff;
   font-size: 1rem;
   margin: 0;
 `;
 
-export const Card = styled.div`
-  background-color: #fff;
-  color: #333;
-  padding: 1rem;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  min-width: 500px;
-  max-width: 800px;
-  margin-bottom: 1rem;
-`;
-
-export const InnerCard = styled.div`
-    background-color: #ffe;
-    color: #333;
-    padding: 1rem;
-    border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-    `;
-
 export const Button = styled.span`
-  background-color: #f0f0ff;
+  background-color: #f0f0ff33;
   padding: 0.5rem 1rem;
   margin: 5px 0;
   border-radius: 4px;
   display: inline-block;
-  color: blue;
   cursor: pointer;
+  user-select: none;
+  color: #dddddd;
 
     &:hover {
-        background-color: #e0e0ff;
+        background-color: #f0f0ff55;
     }
     &:active {
         background-color: #d0d0ff;
@@ -64,8 +75,8 @@ export const Paragraph = styled.p`
 `;
 
 export const Code = styled.code`
-  color: #456236;
-  background-color: #a9c49333;
+  color: #a4ef7d;
+  background-color: #95d56033;
   padding: 0.1rem 0.2rem;
   margin: -0.1rem -0.2rem;
   border-radius: 4px;
